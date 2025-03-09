eb3a85c6b1ef461dfa482a7087990d2fef1cc3f9
Kishore-SCM <kishoredevops681993@gmail.com>
2025-03-09 01:00:58 -0500

    one more commit

--- sample.sh
+++ sample.sh
@@ -9,4 +9,8 @@ echo "You are not eligible to apply vote. kindly apply after $result years"
 fi
 adding new line
 added ssh key in github
+<<<<<<< HEAD
 added this line from GITHUB
+=======
+added this line from linux server
+>>>>>>> 2cd5c4d ( added one line from linux server)
